2ac8d7426319d4aa80688455078eb952c1b44ae0
orsperling <orsp@agri.gov.il>
2025-11-27 15:10:57 +0200

    11

--- IrrApp.py
+++ IrrApp.py
@@ -239,7 +239,7 @@ def save_map_as_image_static(lat, lon, zoom=15, size=(600, 450), marker_path='im
 
 
 # 🌟 **Streamlit UI**
-st.st.markdown("<h1 style='text-align: center;'>G-WaB: Geographic Water Budget</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>G-WaB: Geographic Water Budget</h1>", unsafe_allow_html=True)
 st.markdown(
     "<p style='text-align: center; font-size: 20px'>A <a href=\"https://www.bard-isus.org/\"> <strong>BARD</strong></a> research report by: </p>"
     "<p style='text-align: center;'><a href=\"mailto:orsp@volcani.agri.gov.il\"> <strong>Or Sperling</strong></a> (ARO-Volcani), "
@@ -247,6 +247,7 @@ st.markdown(
     "<a href=\"mailto:zellis@ucdavis.edu\"> <strong>Zac Ellis</strong></a> (UC Davis), "
     "and <a href=\"mailto:niccolo.tricerri@unito.it\"> <strong>Niccolò Tricerri</strong></a> (UNITO - IUSS Pavia)  </p>",
     unsafe_allow_html=True)
+
 
 # 📌 **User Inputs**
 # 🌍 Unit system selection
